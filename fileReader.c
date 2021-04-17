6e9eb7d3fefac8dce7c3077eb423e7f557432910
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-04-17 13:45:15 -0500

    Added a gitignore

--- fileReader.c
+++ fileReader.c
@@ -1,16 +1,37 @@
 #include "structs.h"
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 int main(int argc, char const *argv[]) {
- struct clientInformation c;
- struct clientInformation *info = &c;
+    struct clientInformation c;
+    struct clientInformation *info = &c;
 
- return 0;
+    strcpy(c.ClientName, "Tyler");
+    strcpy(c.DateOfBirth, "6/23/1999");
+    strcpy(c.Gender, "Male");
+    strcpy(c.DateOfTravel, "1/1/2021");
+    c.GovernmentID = 15;
+    c.NumberOfTravelers = 3;
+
+    fileWriter(info);
+
+    return 0;
 }
 
-void fileReader(struct *customer){
+void fileWriter(struct clientInformation *customer){
+    FILE* summary = fopen("Summary.txt", "w");
+    fprintf(summary, "%s, ", &customer->ClientName);
+    fprintf(summary, "%s, ", customer->DateOfBirth);
+    fprintf(summary, "%s, ", customer->Gender);
+    fprintf(summary, "%d, ", customer->GovernmentID);
+    fprintf(summary, "%s, ", customer->DateOfTravel);
+    fprintf(summary, "%d, ", customer->NumberOfTravelers);
+    fprintf(summary, "%d/n", customer->MenuOption);
+
+
+
+
 
 }
