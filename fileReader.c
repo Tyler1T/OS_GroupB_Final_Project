c79ed2447b3435bcf2bc0efd25c416a05a23b210
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-04-17 12:53:30 -0500

    Added file reader and a summary file

---
+++ fileReader.c
@@ -0,0 +1,16 @@
+#include "structs.h"
+#include <stdio.h>
+#include <stlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char const *argv[]) {
+ struct clientInformation c;
+ struct clientInformation *info = &c;
+
+ return 0;
+}
+
+void fileReader(struct *customer){
+
+}
